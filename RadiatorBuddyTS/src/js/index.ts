c603b4eb903ced8707e2da391029f01a0e207814
Weyrun <Mikkelkroyer@hotmail.com>
2018-12-07 13:07:52 +0100

    Added alot
    Cosmetic items, functions and more

--- RadiatorBuddyTS/src/js/index.ts
+++ RadiatorBuddyTS/src/js/index.ts
@@ -7,20 +7,31 @@ let room = new Room;
 let url = window.location.pathname;
 let navigate = url.substring(url.lastIndexOf('/')+1)
 let coll: HTMLCollection = <HTMLCollection> document.getElementsByClassName("collapsible")
-let createroom: HTMLLinkElement = <HTMLLinkElement> document.getElementById("createroom")
+let savecoll: HTMLCollection = <HTMLCollection> document.getElementsByClassName("savebutton")
+let createroom: HTMLButtonElement = <HTMLButtonElement> document.getElementById("createroom")
 let homediv: HTMLDivElement = <HTMLDivElement> document.getElementById("homediv")
 
-function check(){
+async function check(){
     if (navigate === "room.html"){
         //room.getMacadresses();
-        room.getrooms();
+        await room.getrooms();
     }
     else if(navigate === "create.html"){
         room.setupCreate();
-    }
-    
+        createbutton();
+    }  
+    checkopenroom()
+    checksave()
+    //console.log(coll.length)
 }
 
+function createbutton(){
+    createroom.addEventListener("click", function() {
+        room.posttodb();
+    })    
+}
+
+function checkopenroom(){
 for(var i = 0; i < coll.length; i++){
     coll[i].addEventListener("click", function() {
         this.classList.toggle("active")
@@ -33,5 +44,33 @@ for(var i = 0; i < coll.length; i++){
         }
     });
 }
+}
 
+function checksave(){
+    console.log(room.roomarray)
+    for(let i = 0; i < savecoll.length; i++){
+        savecoll[i].addEventListener("click", function(){
+            console.log(i)
+            let location: HTMLInputElement = <HTMLInputElement> document.getElementById("roomid" + i)
+            console.log(location.id)
+            let inorout: HTMLInputElement = <HTMLInputElement> document.getElementById("iuid" + i)
+            console.log(inorout.id)
+            let temperature: HTMLInputElement = <HTMLInputElement> document.getElementById("tempid" + i)
+            console.log(temperature.id)
+            if(location.value === null){
+                location.value = location.placeholder;
+            }
+            if(temperature.value === null){
+                temperature.value = temperature.placeholder;
+            }
+            axios.put(baseURL + '/sensorsdata/rooms', {
+                macAddress: room.roomarray[i],
+                location: location.value,
+                inDoor: inorout.value,
+                optimalTemperature: temperature.value
+            })
+        })
+    }
+}
+checksave()
 check()
--- RadiatorBuddyTS/src/js/room.ts
+++ RadiatorBuddyTS/src/js/room.ts
@@ -3,18 +3,22 @@ import axios, {AxiosRequestConfig, AxiosPromise} from "axios";
 
 export class Room{
     baseURL: string = 'https://radiatorbuddy.azurewebsites.net/api';
-    macadresses: Array<string> = new Array<string>();
+    macadresses: Array<string>;
     sensorSelect: HTMLSelectElement = <HTMLSelectElement> document.getElementById("sensorselect");
     div: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
+    lname: HTMLInputElement = <HTMLInputElement> document.getElementById("lname");
+    InOrOut: HTMLSelectElement = <HTMLSelectElement> document.getElementById("inorout")
+    roomarray: Array<string> = new Array<string>();
 
     trimQuotes (string: string)
     {
         return string.replace(/['"]+/g, '');
     }
 
-    getMacadresses(){
+    async getMacadresses(){
         let temparray: Array<string> = new Array<string>();
-        axios.get(this.baseURL + '/sensorsdata')
+        this.macadresses = new Array<string>();
+        await axios.get(this.baseURL + '/sensorsdata')
         .then(function(responce){
             for(let d of responce.data){
                 if(temparray.indexOf(d.id) === -1){
@@ -23,49 +27,63 @@ export class Room{
             }
         });
         this.macadresses = temparray;
-        console.log(this.macadresses)
     }
 
-    compareMacAdresses(){
+    async compareMacAdresses(){
         let temparray: Array<string> = new Array<string>();
         temparray = this.macadresses;
         let index;
-        axios.get(this.baseURL + '/sensorsdata/rooms')
+        await axios.get(this.baseURL + '/sensorsdata/rooms')
         .then(function(responce){
             for(let i of responce.data){
-                console.log(responce.data)
-                index = temparray.indexOf(i);
+                console.log(i.macAddress)
+                index = temparray.indexOf(i.macAddress);
                 while(index !== -1){
                     temparray.splice(index, 1);
-                    index = temparray.indexOf(i);
+                    index = temparray.indexOf(i.macAddress);
                 }
             }
         })
         this.macadresses = temparray;
-        console.log(this.macadresses)
     }
 
-    setupCreate(){
-        this.getMacadresses();
-        this.compareMacAdresses();
-        console.log(this.macadresses)   
-        for(let i of this.macadresses){
-            let op = this.sensorSelect.appendChild(document.createElement("option"));
-            op.setAttribute("value", i);
-            op.innerText = i.toString();
+    async setupCreate(){        
+        await this.getMacadresses();
+        await this.compareMacAdresses();
+        for(let i in this.macadresses){
+            console.log(this.macadresses[i])
+            let op = document.createElement("option");
+            this.sensorSelect.appendChild(op);
+            op.setAttribute("value", this.macadresses[i]);
+            op.innerText = this.macadresses[i].toString();
         }
+
     }
 
+    posttodb(){
+        axios.post(this.baseURL + '/sensorsdata/rooms', {
+            MacAddress: this.sensorSelect.value,
+            Location: this.lname.value,
+            inDoor: this.InOrOut.value
+        })
+        alert("Rum oprettet")
+    }
     
 
     async getrooms(){
-        let tempdiv: HTMLDivElement = <HTMLDivElement> document.getElementById("thisdivthatdoesnotwork")
+        let tempdiv: HTMLDivElement = <HTMLDivElement> document.getElementById("roomdiv")
+        let temparray: Array<string> = new Array<string>();
+        let number: number = 0;
+        let roomid: string = "roomid" + number;
+        let tempid: string = "tempid" + number;
+        let iuid: string = "iuid" + number;
         await axios.get(this.baseURL + '/sensorsdata/rooms')
         .then(function(responce){
             for (let d of responce.data){
+                temparray.push(d.macAddress);
                 let newbutton = tempdiv.appendChild(document.createElement("button"));
                 newbutton.setAttribute("class", "collapsible")
-                newbutton.innerText = JSON.stringify(d.location)
+                newbutton.innerText = JSON.stringify(d.location).replace(/['"]+/g, '')
                 let newdiv = tempdiv.appendChild(document.createElement("div"));
                 newdiv.setAttribute("class", "content");
                 let table = newdiv.appendChild(document.createElement("table"))
@@ -74,21 +92,63 @@ export class Room{
                 thead.setAttribute("id", "tableitems")
         
                 let trow = thead.appendChild(document.createElement("tr"))
-                trow.appendChild(document.createElement("td")).innerHTML = "Room Name"
+                trow.appendChild(document.createElement("td")).innerHTML = "Rum navn"
                 let td = trow.appendChild(document.createElement("td"))
-                td.appendChild(document.createElement("input")).innerHTML = JSON.stringify(d.location);
+                let input = td.appendChild(document.createElement("input"))
+                input.setAttribute("class", "input")
+                input.setAttribute("id", roomid);
+                input.setAttribute("placeholder", d.location);
 
                 trow = thead.appendChild(document.createElement("tr"))
                 trow.appendChild(document.createElement("td")).innerHTML = "Sensor Mac-Adress"
                 td = trow.appendChild(document.createElement("td"))
-                td.appendChild(document.createElement("input")).innerHTML = JSON.stringify(d.MacAddress)
+                td.setAttribute("id", "mactext")
+                td.innerHTML = d.macAddress
 
                 trow = thead.appendChild(document.createElement("tr"))
-                trow.appendChild(document.createElement("td")).innerHTML = "In or outdoor"
+                trow.appendChild(document.createElement("td")).innerHTML = "ind eller udenfor"
                 td = trow.appendChild(document.createElement("td"))
-                td.appendChild(document.createElement("input")).innerHTML = JSON.stringify(d.inDoor)
+                let select = td.appendChild(document.createElement("select"))
+                select.setAttribute("id", iuid)
+                if (d.inDoor === true){
+                    let option = select.appendChild(document.createElement("option"))
+                    option.setAttribute("value", "true")
+                    option.innerText = "Ja";
+                    option = select.appendChild(document.createElement("option"))
+                    option.setAttribute("value", "false")
+                    option.innerText = "Nej";
+                }
+                else{
+                    let option = select.appendChild(document.createElement("option"))
+                    option.setAttribute("value", "false")
+                    option.innerText = "Nej";
+                    option = select.appendChild(document.createElement("option"))
+                    option.setAttribute("value", "true")
+                    option.innerText = "Ja";
+                }
+
+                trow = thead.appendChild(document.createElement("tr"))
+                trow.appendChild(document.createElement("td")).innerHTML = "Optimal Temperatur"
+                td = trow.appendChild(document.createElement("td"))
+                input = td.appendChild(document.createElement("input"))
+                input.setAttribute("class", "input")
+                input.setAttribute("type", "number")
+                input.setAttribute("id", tempid)
+                input.setAttribute("placeholder", d.optimalTemperature);
+
+                let savebutton = newdiv.appendChild(document.createElement("button"))
+                savebutton.setAttribute("class", "savebutton")
+                
+                savebutton.innerHTML = "Save"
+                number++
+                roomid = "roomid" + number;
+                tempid = "tempid" + number;
+                iuid = "iuid" + number;
             }
         });
+        this.roomarray = temparray;
+
+        console.log(this.roomarray);
     }
     
 }
